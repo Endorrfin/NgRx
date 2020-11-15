5b9d7a1bc77dd187e123503df431f31a622b192b
Vasiliy Krupka <vasiliy.k@payop.com>
2020-11-15 07:42:58 +0200

    Full App with Angular Material, Angularfire & NgRx: L 2.14 - services; Dependency injection

--- my-dream-app/src/app/product/product.component.ts
+++ my-dream-app/src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product',
@@ -9,13 +10,14 @@ export class ProductComponent implements OnInit {
   @Input() productName: string;
   @Output() productClicked = new EventEmitter();
 
-  constructor() { }
+  constructor(private productService: ProductsService) { }
 
   ngOnInit(): void {
   }
 
   onClicked() {
-    this.productClicked.emit();
+    // this.productClicked.emit();
+    this.productService.deleteProduct(this.productName);
   }
 
 }
