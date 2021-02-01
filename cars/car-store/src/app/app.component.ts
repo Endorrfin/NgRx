92ffe3d8767f062fe793bb25a4fd4bde9de1d4a6
Vasiliy Krupka <vasiliy.k@payop.com>
2021-02-01 05:44:14 +0200

    ngrx car-store: unit-2 do functional

--- cars/car-store/src/app/app.component.ts
+++ cars/car-store/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Car } from './car.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'car-store';
+  public cars: Car[] = [
+    new Car('Ford', '02.02.2021', 'Focus', false, 1),
+    new Car('Audi', '02.03.2021', 'A4', false, 2),
+  ];
+
+
+  onAdd(car: Car) {
+    this.cars.push(car);
+  }
+
+  onDelete(car: Car) {
+    this.cars = this.cars.filter(c => c.id !== car.id)
+  }
 }
--- cars/car-store/src/app/app.module.ts
+++ cars/car-store/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,10 +13,11 @@ import { CarComponent } from './car/car.component';
     CarsFormComponent,
     CarComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
