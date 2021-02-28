492ef304af78ab7b24ffbfc8dffca8cff02460ce
Vasiliy Krupka <vasiliy.k@payop.com>
2021-02-28 20:55:43 +0200

    NgRx: cars - server start

---
+++ cars/car-store_ngrx_IV_server/src/app/car/car.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+import { Car } from '../car.model';
+import { Store } from '@ngrx/store';
+import { AppState } from '../redux/app.state';
+import { DeleteCar, UpdateCar } from '../redux/cars.action';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent {
+
+  @Input() car: Car; // параметр car типа Car из модели
+
+  constructor(
+    private store: Store<AppState>
+  ) {
+  }
+
+
+  onBuy() {
+    this.store.dispatch(new UpdateCar(this.car));
+  }
+
+  onDelete() {
+    this.store.dispatch(new DeleteCar(this.car));
+  }
+}
---
+++ cars/car-store_ngrx_IV_server/src/app/cars-form/cars-form.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { Car } from '../car.model';
+import * as moment from 'moment';
+import { Store } from '@ngrx/store';
+import { AppState } from '../redux/app.state';
+import { AddCar } from '../redux/cars.action';
+
+@Component({
+  selector: 'app-cars-form',
+  templateUrl: './cars-form.component.html',
+  styleUrls: ['./cars-form.component.css']
+})
+export class CarsFormComponent {
+
+  private id: number = 2;
+  carName: string = '';
+  carModel: string = '';
+
+  constructor(
+    private store: Store<AppState>
+  ) {
+  }
+
+  onAdd() {
+    if (this.carName === '' || this.carModel === '') {
+      return;
+    }
+
+    this.id = ++this.id;
+
+    const car = new Car(
+      this.carName,
+      moment().format('DD.MM.YY'),
+      this.carModel,
+      false,
+      this.id
+    );
+
+    // this.addCar.emit(car)
+    this.store.dispatch(new AddCar(car));
+
+    this.carName = '';
+    this.carModel = '';
+  }
+
+  onLoad() {
+    // todo
+  }
+}
